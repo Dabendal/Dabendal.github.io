0c6a99866b404127609bf62990ce61913c6cbc20
Dabendal <mbent083@uottawa.ca>
2022-06-30 14:50:14 +0100

    Updates

---
+++ precache-manifest.6ff86eecff0bb2e716ff3c8f917a64ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "397fb5d4ade82177ff3d",
+    "url": "/projectwyp/static/css/main.1ff4dac7.chunk.css"
+  },
+  {
+    "revision": "397fb5d4ade82177ff3d",
+    "url": "/projectwyp/static/js/main.397fb5d4.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/projectwyp/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "80ba0d628fc581762966",
+    "url": "/projectwyp/static/js/runtime~main.80ba0d62.js"
+  },
+  {
+    "revision": "a67900c87541bb62057a33fc7b2025db",
+    "url": "/projectwyp/index.html"
+  }
+];
